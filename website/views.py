69e7ea7a48da3e640a25820068ee44ab58845618
LewisMDillon <lewismdillon1@gmail.com>
2023-07-25 14:36:53 +0000

    Connect urls to display website templates

--- website/views.py
+++ website/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def home(request):
+    return render(request, 'website/home.html')
+
+
+def about(request):
+    return render(request, 'website/about.html')
