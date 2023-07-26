5bd45b56d11146234b9913c9c24dc7ca4658310f
LewisMDillon <lewismdillon1@gmail.com>
2023-07-26 10:52:41 +0000

    Create post model and display posts in home.html

--- website/urls.py
+++ website/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
     path('', views.home, name='website-home'),
     path('about', views.about, name='website-about')
--- website/views.py
+++ website/views.py
@@ -1,10 +1,27 @@
 from django.shortcuts import render
+from .models import Post
 
-# Create your views here.
+# posts = [
+#     {
+#         'author': 'Lewis',
+#         'title': 'Blog Post 1',
+#         'content': 'This is the first post',
+#         'date_posted': '26th July, 2023'
+#     },
+#     {
+#         'author': 'Lewis',
+#         'title': 'Blog Post 2',
+#         'content': 'This is the second post',
+#         'date_posted': '27th July, 2023'
+#     }
+# ]
 
 
 def home(request):
-    return render(request, 'website/home.html')
+    context = {
+        'posts': Post.objects.all()
+    }
+    return render(request, 'website/home.html', context)
 
 
 def about(request):
