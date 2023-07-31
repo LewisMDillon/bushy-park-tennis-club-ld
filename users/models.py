6e442318f2e5b1956fdedf8f40b038a998558db0
LewisMDillon <lewismdillon1@gmail.com>
2023-07-31 14:27:34 +0000

    Add user profile model and basic profile page

--- users/models.py
+++ users/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = CloudinaryField('image', default='default')
+
+    def __str__(self):
+        return f'{self.user.username} Profile'
