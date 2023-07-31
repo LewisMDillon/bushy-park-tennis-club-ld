1674b9d4ca7cfb09a03483732460aaae3fc3ae84
LewisMDillon <lewismdillon1@gmail.com>
2023-07-31 12:33:03 +0000

    Add basic user profile page & login requirement

--- tennisclub/settings.py
+++ tennisclub/settings.py
@@ -154,3 +154,4 @@ CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap4"
 CRISPY_TEMPLATE_PACK = "bootstrap4"
 
 LOGIN_REDIRECT_URL = 'website-home'
+LOGIN_URL = 'login'
--- users/views.py
+++ users/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from .forms import UserRegisterForm
 
 
@@ -17,3 +18,8 @@ def register(request):
     else:
         form = UserRegisterForm()
     return render(request, 'users/register.html', {'form': form})
+
+
+@login_required
+def profile(request):
+    return render(request, 'users/profile.html')
