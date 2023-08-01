2bb98b970a6a7a7d554ee5ab3f1e75eea649a912
LewisMDillon <lewismdillon1@gmail.com>
2023-08-01 09:46:55 +0000

    Add createview for creating new homepage posts

--- website/urls.py
+++ website/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path
-from .views import PostListView, PostDetailView
+from .views import (
+    PostListView,
+    PostDetailView,
+    PostCreateView
+    )
 from . import views
 
 
 urlpatterns = [
     path('', PostListView.as_view(), name='website-home'),
     path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('post/new/', PostCreateView.as_view(), name='post-create'),
     path('about', views.about, name='website-about')
 ]
