4c28d47dd73663abec2a2cf29283482a5bea3114
LewisMDillon <lewismdillon1@gmail.com>
2023-08-01 09:55:41 +0000

    Add update view & login required functionality

--- website/views.py
+++ website/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import (
-    ListView, 
-    DetailView, 
-    CreateView
+    ListView,
+    DetailView,
+    CreateView,
+    UpdateView,
     )
 from .models import Post
 
@@ -25,7 +27,16 @@ class PostDetailView(DetailView):
     model = Post
 
 
-class PostCreateView(CreateView):
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+
+class PostUpdateView(LoginRequiredMixin, UpdateView):
     model = Post
     fields = ['title', 'content']
 
