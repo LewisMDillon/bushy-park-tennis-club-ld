242feef622341447b114315cc7e46790ed45bee3
LewisMDillon <lewismdillon1@gmail.com>
2023-08-01 12:58:11 +0000

    Add root static folder for heroku deployment test

--- tennisclub/settings.py
+++ tennisclub/settings.py
@@ -28,7 +28,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = 'DEBUG' in os.environ
 
 ALLOWED_HOSTS = [
     '8000-lewismdillo-bushyparkte-divi0h8scca.ws-eu102.gitpod.io',
