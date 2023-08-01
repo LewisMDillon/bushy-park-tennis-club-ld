51409f0e72bcbe0f180027a11397028985bc50df
LewisMDillon <lewismdillon1@gmail.com>
2023-08-01 09:29:28 +0000

    Add detail view for homepage posts

--- website/urls.py
+++ website/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import PostListView
+from .views import PostListView, PostDetailView
 from . import views
 
 
 urlpatterns = [
     path('', PostListView.as_view(), name='website-home'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
     path('about', views.about, name='website-about')
 ]
--- website/views.py
+++ website/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Post
 
 
@@ -15,6 +15,10 @@ class PostListView(ListView):
     template_name = 'website/home.html'
     context_object_name = 'posts'
     ordering = ['-date_posted']
+
+
+class PostDetailView(DetailView):
+    model = Post
 
 
 def about(request):
