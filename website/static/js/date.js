ad316684cbb641771a01bc4cee7eee5562a55109
LewisMDillon <lewismdillon1@gmail.com>
2023-08-31 10:55:15 +0000

    Add sevseral comments to date.js

--- website/static/js/date.js
+++ website/static/js/date.js
@@ -1,8 +1,9 @@
-// DATE PICKER
+// DATE PICKER 
 
+// Gets date info for formatting
 var today = new Date();
 var dd = today.getDate();
-var mm = today.getMonth() + 1; //January is 0!
+var mm = today.getMonth() + 1; // January is 0
 var yyyy = today.getFullYear();
 
 if (dd < 10) {
@@ -12,7 +13,9 @@ if (dd < 10) {
 if (mm < 10) {
    mm = '0' + mm;
 } 
-    
+
+// Formats the date
 today = yyyy + '-' + mm + '-' + dd;
 
+// Disables selection of dates before the present day
 document.getElementById("date-picker").setAttribute("min", today);
