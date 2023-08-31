4a63f3dfc5cb5008534ec1f56617df7fce54bf1d
LewisMDillon <lewismdillon1@gmail.com>
2023-08-31 10:57:47 +0000

    Add several comments to profile.js

--- website/static/js/profile.js
+++ website/static/js/profile.js
@@ -1,18 +1,20 @@
 // PROFILE UPDATE
 
+// Gets the update button and adds event listener
 let editButton = document.getElementById('update-button')
-
 editButton.addEventListener("click", showForm)
 
+// Function to display the update form (hidden by default)
 function showForm() {
     let updateForm = document.getElementById("update-form")
     updateForm.style.display = "block"
 }
 
+// Gets the cancel button and adds event listener
 let cancelButton = document.getElementById("update-cancel-button")
-
 cancelButton.addEventListener("click", hideForm)
 
+// Function to hide the update form
 function hideForm() {
     let updateForm = document.getElementById("update-form")
     updateForm.style.display = "none"
