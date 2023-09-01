22e25720a6bac95c89909c8d4415f38eafbb2b15
LewisMDillon <lewismdillon1@gmail.com>
2023-09-01 17:59:45 +0000

    Fix HTML validation errors on all templates

--- users/forms.py
+++ users/forms.py
@@ -19,6 +19,12 @@ class UserRegisterForm(UserCreationForm):
             'password1',
             'password2'
             ]
+    
+    def __init__(self, *args, **kwargs):
+        super(UserRegisterForm, self).__init__(*args, **kwargs)
+
+        for fieldname in ['username', 'password1', 'password2']:
+            self.fields[fieldname].help_text = None
 
 
 class UserUpdateForm(forms.ModelForm):
